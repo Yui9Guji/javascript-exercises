69419e06b2afcb4c6bedde94b5d983e48065b88e
Yui Guji <nikitunzod@gmail.com>
2022-08-01 21:25:03 +0300

    Ex. 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let addition = ''
+    for (let i = 0; i < num; i++) {
+        addition += string
+    }
+    return addition
 };
 
 // Do not edit below this line
