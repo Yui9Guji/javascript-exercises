7a54e55424677d0761c925d699a88fad49042ca8
Yui Guji <nikitunzod@gmail.com>
2022-08-02 01:53:58 +0300

    Ex. 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(varOne, varTwo) {
+    let sum = 0
 
+    if (varOne < 0 || varTwo < 0 || typeof varOne !== 'number' || typeof varTwo !== 'number') {
+        return 'ERROR'
+    }
+    else if (varOne < varTwo) {
+        for (let i = varOne; i < varTwo + 1; i++) {
+            sum += i
+        }
+    }
+
+    else {
+        for (let i = varTwo; i < varOne + 1; i++) {
+            sum += i
+        }
+    }
+
+    return sum
 };
 
 // Do not edit below this line
