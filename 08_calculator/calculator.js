240259a1344fc93ecd9e0e5e62564c20eeb00969
Yui Guji <nikitunzod@gmail.com>
2022-08-01 21:04:35 +0300

    Ex. 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((a, b) => a + b)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((a, b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+    let number = 1
+	for (let i = 1; i < a; i++) {
+        number *= i
+    }
+    return number
 };
 
 // Do not edit below this line
