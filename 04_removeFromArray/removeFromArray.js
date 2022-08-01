2272425798ea7d96c14aa49ac0d896608f306221
Yui Guji <nikitunzod@gmail.com>
2022-08-01 22:17:18 +0300

    Ex. 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, one, two) {
 
+    let indexOne = array.indexOf(one)
+    let indexTwo = array.indexOf(two)
+
+    if (indexOne < indexTwo) {
+        let res = [indexOne, indexTwo]
+        indexOne = res[1]
+        indexTwo = res[0]
+    }
+
+    if (indexOne > -1) {
+        array.splice(indexOne, 1)
+    }
+    if (indexTwo > -1) {
+        array.splice(indexTwo, 1)
+    }
+
+    return array
 };
 
 // Do not edit below this line
