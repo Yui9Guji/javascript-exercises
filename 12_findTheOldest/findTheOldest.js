74aab4012374b5fc798ebec1b26192085a59afff
Yui Guji <nikitunzod@gmail.com>
2022-08-02 22:07:41 +0300

    Ex. 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    let years = []
+
+    array.forEach(obj => {
+        if (obj.yearOfDeath !== undefined) {
+            years.push(obj.yearOfDeath - obj.yearOfBirth)
+        }
+
+        else {
+            const date = new Date()
+            let yearsLived = date.getFullYear() - obj.yearOfBirth
+            years.push(yearsLived)
+        }
+    })
+
+    const maxYear = years.reduce((a, b) => Math.max(a, b))
+
+    return array[years.indexOf(maxYear)]
 };
 
 // Do not edit below this line
