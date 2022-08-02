8f48d3556dcacdcbeac59c7a8b8aa9a3395a0148
Yui Guji <nikitunzod@gmail.com>
2022-08-02 21:55:38 +0300

    Ex. 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
 
+    let books = []
+
+    array.forEach(variable => {
+        books.push(variable.title)
+    })
+
+    return books
 };
 
 // Do not edit below this line
