e5bc21c0b0e9de68fb1856eaa61c537c79457317
Yui Guji <nikitunzod@gmail.com>
2022-08-02 21:30:19 +0300

    Ex. 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const word = string.toLowerCase()
+    const array = word.split('')
 
+    array.forEach(syllable => {
+        if (syllable === '!' || syllable === '.'
+            || syllable === ',' || syllable === '?'
+            || syllable === '&' || syllable === ':'
+            || syllable === ' ') {
+                array.splice(array.indexOf(syllable), 1)
+        }
+    })
+
+    array.forEach(syllable => {
+        if (syllable === '!' || syllable === '.'
+            || syllable === ',' || syllable === '?'
+            || syllable === '&' || syllable === ':'
+            || syllable === ' ') {
+            array.splice(array.indexOf(syllable), 1)
+        }
+    })
+    
+    const middleAnswer = array.join('')
+    const reverse = array.reverse()
+    const answer = reverse.join('')
+
+    console.log(middleAnswer)
+    console.log(answer)
+
+    return answer === middleAnswer;
 };
-
+palindromes('A car, a man, a maraca.')
 // Do not edit below this line
 module.exports = palindromes;
